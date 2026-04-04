b952d419f360c6235a98a9af3a363d5978ce83b9
ruxshona <ruxshonaaglamkhujayeva@gmail.com>
2026-04-04 13:31:10 +0500

    changed

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -2,12 +2,11 @@ from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 
 from app.core.security import (
-    create_access_token, 
+    create_access_token,
     create_refresh_token,
     get_password_hash,
     verify_password,
 )
-
 from app.models.user import User
 from app.schemas.auth import LoginRequest, RegisterRequest, TokenResponse
 
@@ -15,19 +14,17 @@ from app.schemas.auth import LoginRequest, RegisterRequest, TokenResponse
 class AuthService:
     def __init__(self, db: Session):
         self.db = db
-    
+
     def register(self, data: RegisterRequest) -> TokenResponse:
         existing = self.db.query(User).filter(User.email == data.email).first()
-
         if existing:
-            raise HTTPException(
-                status_code = 409, detail= "Bu email allaqachon mavjud"
-            )
+            raise HTTPException(status_code=409, detail="Bu email allaqachon mavjud")
+
         user = User(
-            name=data.name, 
-            email=data.email, 
-            password=get_password_hash(data.password), 
-            role="user",
+            name=data.name,
+            email=data.email,
+            password=get_password_hash(data.password),
+            role=data.role.value,   # ← enum dan string ga
         )
         self.db.add(user)
         self.db.commit()
@@ -37,10 +34,9 @@ class AuthService:
             access_token=create_access_token(str(user.id)),
             refresh_token=create_refresh_token(str(user.id)),
         )
-    
+
     def login(self, data: LoginRequest) -> TokenResponse:
         user = self.db.query(User).filter(User.email == data.email).first()
-        
         if not user or not verify_password(data.password, user.password):
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
